700f3cab3fe53a62dec1158460b6e5dc85d04162
rmxrmx <mrokas@protonmail.com>
2022-06-23 16:51:56 +0200

    update

--- main.py
+++ main.py
@@ -1,19 +1,47 @@
+from ntpath import join
+from turtle import onclick
 from numpy import number
 import requests
 import wikipediaapi as wp
 import streamlit as st
 import random
 
-#st.set_page_config(layout="wide")
-wiki_wiki = wp.Wikipedia('en')
+# index == the 10 pages for questions / answers
+# page:
+#   0: starting page
+#   1: game page
+#   2: results page
+if 'index' not in st.session_state:
+    st.session_state['index'] = 0
+if 'page' not in st.session_state:
+    st.session_state['page'] = 0
+if "results" not in st.session_state:
+    st.session_state["results"] = []
+if "game_number" not in st.session_state:
+    st.session_state["game_number"] = 0
+
+
+def advance_game():
+    st.session_state["page"] += 1
+    if "lang" not in st.session_state:
+        st.session_state["lang"] = st.session_state["language"]
+    st.session_state["index"] = 0
+
+if st.session_state["page"] == 0:
+    st.title("Learning with Wikipedia!")
+    st.selectbox("Language", ["en", "da", "de", "ru"], key="language")
+    st.button("Start", on_click=advance_game)
+
+
+if st.session_state["page"] == 1:
+    language = st.session_state["lang"]
+    wiki_wiki = wp.Wikipedia(language)
 
 
 def print_sections(sections, level=0):
         for s in sections:
                 print("%s: %s - %s" % ("*" * (level + 1), s.title, s.text))
 
-
-#print_sections(page_py.sections)
 
 def print_categorymembers(categorymembers, level=0, max_level=1):
         for c in categorymembers.values():
@@ -25,174 +53,9 @@ def print_categorymembers(categorymembers, level=0, max_level=1):
 
 S = requests.Session()
 
-URL = "https://en.wikipedia.org/w/api.php"
-
-# PARAMS = {
-#     "action": "query",
-#     "format": "json",
-#     "list": "allcategories",
-#     "aclimit": "max",
-#     "acmin": 30
-# }
-
-# R = S.get(url=URL, params=PARAMS)
-# DATA = R.json()
-# next_cat = DATA["continue"]["accontinue"]
-# cats = DATA["query"]["allcategories"]
-
-# categories = []
-# for cat in cats:
-#     categories.append(cat["*"])
 
 
-# while True:
-#     PARAMS = {
-#     "action": "query",
-#     "format": "json",
-#     "list": "allcategories",
-#     "aclimit": "max",
-#     "acmin": 30,
-#     "accontinue": next_cat
-#     }
-
-#     R = S.get(url=URL, params=PARAMS)
-#     DATA = R.json()
-#     cats = DATA["query"]["allcategories"]
-
-#     for cat in cats:
-#         categories.append(cat["*"])
-
-#     if "continue" in DATA:
-#         next_cat = DATA["continue"]["accontinue"]
-#     else:
-#         break
-
-
-# # print(categories)
-
-# # Now we have all of the categories of some relevance
-
-# translated_categories = []
-# i = 0
-
-# while i < len(categories):
-#     PARAMS = {
-#         "action": "query",
-#         "titles": "|".join(categories[i:i+30]),
-#         "prop": "langlinks",
-#         "format": "json",
-#         "lllimit": "max",
-#         "lllang": "en"
-#     }
-
-#     R = S.get(url=URL, params=PARAMS)
-#     DATA = R.json()
-
-#     for link in DATA["query"]["pages"].values():
-#         if "langlinks" in link:
-#             translated_categories.append([link["title"], link["langlinks"][0]["*"]])
-
-#     i += 30
-
-# print(translated_categories)
-
-# Now we have any useful categories + their translation to English
-
-
-
-
-
-# page_extracts = []
-# page_categories = []
-# for title in page_titles:
-#     # get extract of a page
-#     PARAMS = {
-#         "action": "query",
-#         "format": "json",
-#         "prop": "extracts",
-#         "exchars": 400,
-#         "exintro": True,
-#         "titles": title,
-#         "explaintext": True
-#     }
-
-#     R = S.get(url=URL, params=PARAMS)
-#     texts = R.json()
-
-#     page_extracts.append(list(texts["query"]["pages"].values())[0]["extract"])
-
-#     # get categories of a page
-#     PARAMS = {
-#         "action": "query",
-#         "format": "json",
-#         "prop": "categories",
-#         "titles": title,
-#         "cllimit": 10,
-#         "clshow": "!hidden"
-#     }
-
-#     R = S.get(url=URL, params=PARAMS)
-#     categories = R.json()
-
-#     # choose random category
-#     #number_of_categories = len(list(categories["query"]["pages"].values())[0]["categories"])
-
-
-#     # ignores "born in" and "died in" categories
-#     cats = []
-#     for cat in list(categories["query"]["pages"].values())[0]["categories"]:
-#         if "Født" not in cat["title"] and "Døde" not in cat["title"]:
-#             cats.append(cat["title"])
-
-#     chosen_category = random.randint(0, len(cats) - 1)
-#     page_categories.append(cats[chosen_category])
-
-
-# for i in range(len(page_categories)):
-#     page_categories[i] = [i, page_categories[i]]
-
-# random.shuffle(page_categories)
-
-# st.write(page_titles, page_extracts, page_categories)
-
-# col1, col2, col3 = st.columns([4,2,4])
-
-# for i, title in enumerate(page_titles):
-#     with col1.expander(str(i) + ". " + title):
-#         st.write(page_extracts[i])
-
-# for i, cat in enumerate(page_categories):
-#         col2.number_input("", key=i)
-#         col3.write(cat[1][9:])
-
-# TODO: remove dead and born categories...
-
-
-# cat = wiki_wiki.page("Kategori:Den danske guldalder")
-# print("Category members: Den danske guldalder")
-
-# titles = []
-
-# for c in cat.categorymembers.values():
-#     # get only articles with a good number of sections
-#     # if len(c.sections) >= 4 and c.ns == 0:
-#     titles.append(c.title)
-
-# # print(len(cat.categorymembers.values()), len(titles))
-# random.shuffle(titles)
-# # chosen = random.sample(range(len(titles)), 10)
-# print(chosen)
-
-
-# big_pages = []
-# i = 0
-# while len(big_pages) < 10 or i < len(titles):
-#     page = wiki_wiki.page(titles[i])
-#     if len(page.sections) >= 5 and c.ns == 0:
-#         big_pages.append(page)
-
-# @st.cache(suppress_st_warning=True)
-# def create_text():
+col1, col2 = st.columns(2)
 
 # get 10 random pages
 PARAMS = {
@@ -204,56 +67,143 @@ PARAMS = {
     "rnnamespace": 0
 }
 
-R = S.get(url=URL, params=PARAMS)
-pages = R.json()
+@st.cache(allow_output_mutation=True)
+def get_pages(language, number):
+    URL = "https://" + language + ".wikipedia.org/w/api.php"
+    R = S.get(url=URL, params=PARAMS)
+    pages = R.json()
+    
+    page_summaries = []
+    page_titles = []
 
-page_titles = []
+    for idx, page in enumerate(pages["query"]["random"]):
+        wiki_page = wiki_wiki.page(page["title"])
+        # TODO: might want to do some more extensive splitting
+        page_summaries.append(wiki_page.summary.split()[:200])
+        page_titles.append(str(idx + 1) + ". " + page["title"])
 
-for page in pages["query"]["random"]:
-    page_titles.append(page["title"])
+    return page_titles, page_summaries
 
-# TODO: make this work for all of them
-page = wiki_wiki.page(page_titles[0])
-max_len = 0
-max_section = ""
-max_title = ""
-for section in page.sections:
-    if len(section.text) > max_len:
-        max_len = len(section.text)
-        max_section = section.text
-        max_title = section.title
-        # with st.expander(section.title):
-        #     st.write(section.text)
-
-words = max_section.split()
-
-chosen_words = random.sample(range(len(words)), 5)
-chosen_words.sort()
-
-removed_words = [words[i] for i in chosen_words]
-
-for i, index in enumerate(chosen_words):
-    words[index] = str(i+1) + ".___"
-
-col1, col2 = st.columns(2)
-random.shuffle(removed_words)
+if st.session_state["page"] == 1:
+    titles, summaries = get_pages(language, st.session_state["game_number"])
 
 
-words = " ".join(words)
+index = st.session_state['index']
+
+@st.cache(allow_output_mutation=True)
+def generate_page(index):
+    words = summaries[index].copy()
+    print("ran")
+    chosen_words = random.sample(range(len(words)), 5)
+    chosen_words.sort()
+
+    removed_words = [words[i] for i in chosen_words]
+    removed_words_index = [0, 1, 2, 3, 4]
+
+    for i, idx in enumerate(chosen_words):
+        words[idx] = "<font color='orange'>" + str(i+1) + ".\_\_\_</font>"
+
+    random.shuffle(removed_words_index)
 
 
-col1.subheader(page.title + " / " + max_title)
-col1.write(words)
+    joined_words = " ".join(words)
 
-form = col2.form("my_form")
+    return removed_words, removed_words_index, joined_words, words, chosen_words
 
-for word in removed_words:
-    form.number_input(word, 1, 5, step=1, format="%i", key=word)
+if st.session_state["page"] == 1:
+    removed_words, removed_words_index, joined_words, words, chosen_words = generate_page(index)
 
-form.form_submit_button("Submit")
-# for i in chosen:
-#     page = wiki_wiki.page(titles[i])
 
-#     for section in page.sections:
-#          with st.expander(section.title):
-#              st.write(section.text)
+    col1.subheader(titles[index])
+    col1.markdown(joined_words, True)
+
+    form = col2.form("my_form")
+
+    numbered_inputs = []
+
+    for idx, element in enumerate(removed_words_index):
+        form.number_input(removed_words[element], 1, 5, step=1, format="%i", key=str(index) + str(idx))
+
+    for i in range(5):
+        if str(index) + str(i) not in st.session_state:
+            st.session_state[str(index) + str(i)] = 1
+
+def update_page():
+    results = 0
+    for i in range(5):
+        if st.session_state[str(index) + str(i)] == removed_words_index[i] + 1:
+            words[chosen_words[removed_words_index[i]]] = "<font color='green'>" + removed_words[removed_words_index[i]] + "</font>"
+            results += 1
+        else:
+            words[chosen_words[removed_words_index[i]]] = "<font color='red'>~~" + removed_words[st.session_state[str(index) + str(i)] - 1] + "~~ " + removed_words[removed_words_index[i]] + "</font>"        
+    
+
+    j_w = " ".join(words)
+    st.session_state[index] = [j_w, titles[index]]
+
+    st.session_state["results"].append(results)
+
+
+    if st.session_state['index'] == 9:
+        advance_game()
+    else:
+        st.session_state['index'] += 1
+
+def go_back():
+    st.session_state['index'] -= 1
+    
+def update_results_page():
+    st.session_state['index'] += 1
+
+def start_again():
+    st.session_state['page'] = 0
+    st.session_state['index'] = 0
+    st.session_state["game_number"] += 1
+
+if st.session_state["page"] == 1:
+    if index != 9:
+        submit = form.form_submit_button("Next", on_click=update_page)
+    else:
+        submit = form.form_submit_button("Submit", on_click=update_page)
+
+
+if st.session_state["page"] == 2:
+
+    col4, col5 = st.columns(2)
+
+    total_score = sum(st.session_state["results"])
+
+    col4.subheader(st.session_state[index][1])
+    col4.markdown(st.session_state[index][0], True)
+
+    col5.subheader("Results:")
+    col5.write("Total score: {} / 50, or {}%".format(total_score, int(100 * total_score / 50)))
+
+    col1, col2, col3 = st.columns([1, 1, 4])
+    if st.session_state['index'] != 0:
+        col1.button("Back", on_click=go_back)
+    if st.session_state['index'] != 9:
+        col2.button("Next", on_click=update_results_page)
+
+    st.button("Play Again", on_click=start_again)
+
+
+# TODO:
+
+# 1. Make an intro page, where you can choose:
+# language, optionally: category
+
+# 2. in the game:
+# upon submission, allow user to go through their results
+# show some stats
+# button to play again
+# share?
+# high scores?
+
+# have a timer
+
+# VISUAL considerations
+
+# have something to back up your choices
+
+# justify having only one game
